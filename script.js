ee791d49ea40cb190e1af2111d70ec4251b1324e
Brandon Unzueta <unzueta.brandon@gmail.com>
2020-07-16 20:29:44 -0700

    scripts for single meal info

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ function searchMeal(e) {
   //CLEAR SINGLE MEAL
   single_mealEl.innerHTML = "";
 
+  resultHeading.innerHTML = "";
+
   //GET SEARCH TEARM
   const term = search.value;
 
@@ -45,5 +47,65 @@ function searchMeal(e) {
   }
 }
 
+//FETCH MEAL BY ID
+function getMealById(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    });
+}
+
+//ADD MEAL TO DOM
+function addMealToDOM(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i < 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} -${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  single_mealEl.innerHTML = `
+  <div class="single-meal">
+    <h1>${meal.strMeal}</h1>
+    <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+    <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+    </div>
+    <div class="main">
+        <p>${meal.strInstructions}</p>
+        <ul>
+            ${ingredients
+              .map((ingredient) => `<li>${ingredient}</li>`)
+              .join("")}
+        </ul>
+    </div>
+  </div>
+  `;
+}
+
 //EVENT LISTENER
 submit.addEventListener("submit", searchMeal);
+
+mealsEl.addEventListener("click", (e) => {
+  const mealInfo = e.path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains("meal-info");
+    } else {
+      return false;
+    }
+  });
+
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute("data-mealid");
+    getMealById(mealID);
+  }
+});
